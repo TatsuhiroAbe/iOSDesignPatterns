e44855e7ac1786b7171ee47ddafec571e03e621f
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2019-11-22 14:38:44 +0900

    Initial Commit

---
+++ iOSDesignPatterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDesignPatterns
+//
+//  Created by 阿部竜大 on 2019/11/22.
+//  Copyright © 2019 阿部竜大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
