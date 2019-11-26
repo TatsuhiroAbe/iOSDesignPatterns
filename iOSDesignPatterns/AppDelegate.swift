9a3f68c20f9fec0d5321368001af4e7678ea774b
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2019-11-26 11:55:13 +0900

    add views

--- iOSDesignPatterns/AppDelegate.swift
+++ iOSDesignPatterns/AppDelegate.swift
@@ -11,10 +11,13 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
+    private let actionCreator = RepositoryActionCreator()
+    private let repositoryStore = RepositoryStore.shared
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
         return true
     }
 
